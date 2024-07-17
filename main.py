3f7e614737e493624eb557fca2e1fae2d7fa7b44
Taku Ishikura <taku.ishikura@codecomplete.jp>
2024-07-17 11:11:49 +0700

    change order

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import chardet
 import gspread
 import requests
 from selenium import webdriver
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from oauth2client.service_account import ServiceAccountCredentials
 
 
